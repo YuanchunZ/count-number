54267ef071cb69cc88b14bc395fcbe32078f0716
YuanchunZ <102001971+YuanchunZ@users.noreply.github.com>
2022-04-13 22:12:18 -0500

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -9,30 +9,11 @@ Created on Wed Apr 13 16:14:53 2022
 import streamlit as st
 import pandas as pd
 import numpy as np
-#from TextBlob import TextBlob
-#i = 0
-#def count():
-    #global i
-#    i=i+1
-#    st.write(i)
-    #return i
-#st.title("COUNT")
-#result=st.button("increment")
-#st.success(count(0))
-#if st.button("analyze"):
-#     #st.success(count())
-#     global i
-#     i = i + 1
-#     st.write(i)
-     #st.write(result)
+
     
 st.title('Counter Example')
 
-# Streamlit runs from top to bottom on every iteraction so
-# we check if `count` has already been initialized in st.session_state.
 
-# If no, then initialize count to 0
-# If count is already initialized, don't do anything
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
@@ -41,17 +22,13 @@ increment = st.button('Increment')
 if increment:
     st.session_state.count += 1
 
-# A button to decrement the counter
-#decrement = st.button('Decrement')
-#if decrement:
-#    st.session_state.count -= 1
+
 
 st.write('Count = ', st.session_state.count)
 st.write("squared compute")
 x=st.slider('x')
 st.write(x,'squared is', x * x)
 def main():
-    
 
     st.subheader("Natural Language processing:")
     if st.checkbox("Token and lemma"):
@@ -59,11 +36,6 @@ def main():
        message = st.text_area("enter","type here")
        if st.button("analyze"):
            st.success(message.title())
-        
-#read_and_cache_csv = st.cache(pd.read_csv)
-#BUCKET = "https://streamlit-self-driving.s3-us-weat-2.amazonaws.com/"
-#data = read_and_cache_csv(BUCKET + "labels.csv.gz" ,  nrows=1000)
-#desired_lable = st.selectbox('filter to:', ['car','truck'])
-#st.write(data[data.label==desired_label])
+
 if __name__ == '__main__':
       main()    
